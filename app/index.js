e4d1d1f4d317fcbf7969ba97aaf44d740dd4fc2e
Tyler Henkel <tylerhenkel@hotmail.com>
2014-08-19 22:23:00 -0600

    add configs for paths to templates

--- app/index.js
+++ app/index.js
@@ -4,24 +4,34 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
-
 var NgComponentGenerator = yeoman.generators.Base.extend({
 
   info: function () {
-    if(!this.options['skip-message']) {
+    if (!this.options['skip-message']) {
       console.log(chalk.magenta('You\'re using the fantastic NgComponent generator.\n'));
       console.log(chalk.magenta('Initializing yo-rc.json configuration.\n'));
     }
   },
 
-  saveConfig: function() {
-    this.config.set('routeDirectory', this.options.routeDirectory || 'app/components/');
-    this.config.set('directiveDirectory', this.options.directiveDirectory || 'app/components/');
-    this.config.set('filterDirectory', this.options.filterDirectory || 'app/components/');
-    this.config.set('serviceDirectory', this.options.serviceDirectory || 'app/components/');
-    this.config.set('basePath', this.options.basePath || 'app');
-    this.config.set('filters', this.options.filters || ['uirouter']);
-    this.config.set('extensions', this.options.extensions || ['js', 'html', 'scss']);
+  saveConfig: function () {
+    this.config.defaults({
+      'routeDirectory': this.options.routeDirectory || 'app/components/',
+      'directiveDirectory': this.options.directiveDirectory || 'app/components/',
+      'filterDirectory': this.options.filterDirectory || 'app/components/',
+      'serviceDirectory': this.options.serviceDirectory || 'app/components/',
+      'basePath': this.options.basePath || 'app',
+      'filters': this.options.filters || ['uirouter'],
+      'extensions': this.options.extensions || ['js', 'html', 'scss'],
+      'directiveSimple': this.options.directiveSimple || '',
+      'directiveComplex': this.options.directiveComplex || '',
+      'filter': this.options.filter || '',
+      'service': this.options.service || '',
+      'factory': this.options.factory || '',
+      'controller': this.options.controller || '',
+      'decorator': this.options.decorator || '',
+      'provider': this.options.provider || '',
+      'route': this.options.route || ''
+    });
   }
 });
 
