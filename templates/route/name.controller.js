fa97f47c995e0f20ca62f9cc9541b77fa20dfcbd
carpaij <carpaij@gmail.com>
2016-06-17 11:25:49 +0200

    style(route): indentation correction

--- templates/route/name.controller.js
+++ templates/route/name.controller.js
@@ -1,20 +1,21 @@
 'use strict';
+
 (function(){
 <% if(hasFilter('es6')) { %>
-class <%= classedName %>Component {
-  constructor() {
-    this.message = 'Hello';
-  }
+  class <%= classedName %>Component {
+    constructor() {
+      this.message = 'Hello';
+    }
 }<% } else { %>
-function <%= classedName %>Component() {
-  this.message = 'Hello';
+  function <%= classedName %>Component() {
+    this.message = 'Hello';
 }<% } %>
 
-angular.module('<%= scriptAppName %>')
-  .component('<%= cameledName %>', {
-    templateUrl: '<%= htmlUrl %>',
-    controller: <%= classedName %>Component,
-    controllerAs: <%= classedName %>
-  });
+  angular.module('<%= scriptAppName %>')
+    .component('<%= cameledName %>', {
+      templateUrl: '<%= htmlUrl %>',
+      controller: <%= classedName %>Component,
+      controllerAs: <%= classedName %>
+    });
 
 })();
