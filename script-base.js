3533befbb5ae27e596acd6e625a711ec7971217f
Andrew Koroluk <koroluka@gmail.com>
2016-07-01 11:47:49 -0400

    fix(gen): fix yo check

--- script-base.js
+++ script-base.js
@@ -5,9 +5,30 @@ var lodash = require('lodash');
 var s = require('underscore.string');
 var yeoman = require('yeoman-generator');
 var ngUtils = require('./util.js');
+var Promise = require('bluebird');
+var exec = require('child_process').exec;
+var semver = require('semver');
 
 // extend lodash with underscore.string
 lodash.mixin(s.exports());
+
+/**
+ * Run the given command in a child process
+ * @param {string} cmd - command to run
+ * @returns {Promise}
+ */
+function runCmd(cmd) {
+  return new Promise(function(resolve, reject) {
+    exec(cmd, {}, function(err, stdout, stderr) {
+      if(err) {
+        console.error(stdout);
+        return reject(err);
+      } else {
+        return resolve(stdout);
+      }
+    });
+  });
+}
 
 var Generator = module.exports = function Generator() {
   yeoman.Base.apply(this, arguments);
@@ -15,13 +36,18 @@ var Generator = module.exports = function Generator() {
 
   this.lodash = lodash;
 
+  var yoCheckPromise;
+
+  // CI won't have yo installed
   if(!process.env.CI) {
-    yoCheckPromise = genUtils.runCmd('yo --version').then(function(stdout) {
+    yoCheckPromise = runCmd('yo --version').then(function(stdout) {
       if(!semver.satisfies(semver.clean(stdout), '>= 1.7.1')) {
         throw new Error('ERROR: You need to update yo to at least 1.7.1 (npm i -g yo)');
       }
     });
-  } // CI won't have yo installed
+  } else {
+    yoCheckPromise = Promise.resolve();
+  }
 
   try {
     this.appname = require(path.join(process.cwd(), 'bower.json')).name;
@@ -55,6 +81,8 @@ var Generator = module.exports = function Generator() {
   }
 
   this.sourceRoot(path.join(__dirname, '/templates'));
+
+  return yoCheckPromise;
 };
 
 util.inherits(Generator, yeoman.Base);
